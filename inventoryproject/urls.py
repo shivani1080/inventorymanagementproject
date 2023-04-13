b0d40c6e6277228399632762e4243bdd81b6d334
Shivani Singh <shivanisingh8036@gmail.com>
2023-04-13 18:01:59 +0530

    confirmmailchange

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
 from user import views as user_view
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+
 from django.conf.urls.static import static
 urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/',user_view.register,name='user-register'),
+    path('confirm-email/<int:user_id>/<str:token>/', user_view.confirm_email, name='user-confirm-email'),
     path('',auth_views.LoginView.as_view(template_name='user/login.html'),name='user-login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='user-logout'),
     path('profile/',user_view.profile,name='user-profile'),
--- user/views.py
+++ user/views.py
@@ -1,23 +1,63 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import get_user_model
+
+
+
+
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm,userupdateform,profileupdateform
+from .forms import CreateUserForm, userupdateform, profileupdateform
 from django.contrib import messages
 # Create your views here.
+
+User = get_user_model()
+
 
 def register(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            username=form.cleaned_data.get('username')
-            messages.success(request,f'Account has been created for {username}.')
+            
+            username = form.save(commit=False)
+            username.is_active = False  # Set the user as inactive until they confirm their email address
+            username.save()
+            # Generate the confirmation token and send the email
+            token_generator = default_token_generator
+            email_subject = 'Confirm Your Email Address'
+            email_message = f'Please click the link below to confirm your email address:\n\n{request.build_absolute_uri("/confirm-email/" + str(username.pk) + "/" + token_generator.make_token(username))}/'
+            send_mail(email_subject, email_message, settings.DEFAULT_FROM_EMAIL, [username.email], fail_silently=True)
+
+            messages.success(request, f'An email has been sent to {username.email} with instructions to confirm your email address.')
             return redirect('user-login')
+            #username = form.save()
+            #username=form.cleaned_data.get('username')
+            #messages.success(request,f'Account has been created for {username}.')
+            #return redirect('user-login')
     else:
         form=CreateUserForm()
     context={
         'form':form,
     }
     return render(request,'user/register.html',context)
+
+def confirm_email(request, user_id, token):
+    user = User.objects.filter(pk=user_id, is_active=False).first()
+    if user is None:
+        messages.error(request, 'Invalid confirmation link.')
+        return redirect('user-login')
+
+    if default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your email address has been confirmed. You can now log in.')
+    else:
+        messages.error(request, 'Invalid confirmation link.')
+
+    return redirect('user-login')
+
+
 
 def profile(request):
     return render(request,'user/profile.html')
